e7eac4ade325cfd39d66b68b15238996ada0b1b2
bhunao <bh.felix@hotmail.com>
2024-01-23 22:07:05 -0300

    first commit

---
+++ entity.py
@@ -0,0 +1,20 @@
+from typing import List, Tuple
+import pygame
+from dataclasses import dataclass
+
+
+@dataclass
+class Entity:
+    pos: Tuple[int] = (0, 0)
+    speed: Tuple[int] = (0, 0)
+
+    def draw(self, screen, rect=None):
+        color = (255, 0, 0)
+        if rect is None:
+            rect = pygame.Rect(self.x, self.y, 100, 100)
+        pygame.draw.rect(screen, color, rect)
+
+    def update(self):
+        new_x = self.pos[0] * self.speed[0]
+        new_y = self.pos[1] * self.speed[1]
+        self.pos =1 
---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+from entity import Entity
+
+pygame.init()
+
+
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+
+running = True
+one = Entity(speed=[1, 0])
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((255, 255, 255))
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    one.draw(screen)
+
+
+    pygame.display.flip()
+
+pygame.quit()
