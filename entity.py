89ff5f97e8f7547170ed322f96519e558059f4e2
bhunao <bh.felix@hotmail.com>
2024-01-23 23:46:52 -0300

    plataforms and collission

--- entity.py
+++ entity.py
@@ -1,42 +1,61 @@
+from typing import Tuple
 import pygame
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
+from pygame.rect import Rect
+from pygame import draw
+
+
+def default_rect():
+    return pygame.Rect(0, 0, 25, 25)
+
+
+def rgb(offset=0):
+    n = (pygame.time.get_ticks() + offset) % 255
+    r = n
+    g = (n + 85) % 255
+    b = (n + 170) % 255
+    return r, g, b
+
 
 
 @dataclass
 class Entity:
-    x: int = 0
-    y: int = 0
-    speedx: int = 0
-    speedy: int = 0
+    rect: Rect = field(default_factory=default_rect)
+    color: Tuple[int, int, int] = (255, 0, 0)
 
-    def draw(self, screen, rect=None):
-        color = (255, 0, 0)
-        if rect is None:
-            rect = (self.x, self.y, 25, 25)
-        pygame.draw.rect(screen, color, rect)
-
-    def update(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            self.x -= self.speedx
-        if keys[pygame.K_RIGHT]:
-            self.x += self.speedx
-        if keys[pygame.K_UP]:
-            self.y -= self.speedy
-        if keys[pygame.K_DOWN]:
-            self.y += self.speedy
-
-
-@dataclass
-class StaticEntity:
-    x: int = 0
-    y: int = 0
-
-    def draw(self, screen, rect=None):
-        color = (255, 255, 0)
-        if rect is None:
-            rect = (self.x, self.y, 25, 25)
-        pygame.draw.rect(screen, color, rect)
+    def draw(self, screen):
+        draw.rect(screen, self.color, self.rect)
 
     def update(self):
         pass
+
+
+@dataclass
+class StaticEntity(Entity):
+    def draw(self, screen):
+        r, g, b = rgb(100)
+
+        outline_rect = self.rect.copy()
+        outline_rect.x -= 2
+        outline_rect.y -= 2
+        outline_rect.width = self.rect.width + 4
+        outline_rect.height = self.rect.height + 4
+
+        draw.rect(screen, (r, g, b), outline_rect)
+        draw.rect(screen, self.color, self.rect)
+
+    def update(self):
+        pass
+
+
+@dataclass
+class MovableEntity(Entity):
+    speedx: int = 0
+    speedy: int = 0
+    gravity: int = 1
+    is_jumping: bool = False
+
+    def update(self):
+        r, g, b = rgb()
+        self.color = r, g, b
--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+import logging
+from typing import List
 import pygame
-from entity import Entity, StaticEntity
+from pygame.rect import Rect
+from entity import Entity, MovableEntity, StaticEntity
+
+logger = logging.getLogger(__name__)
+
+
+def keyboard_movement(player: MovableEntity, collidable_enties: List[Entity]):
+    keys = pygame.key.get_pressed()
+    old_x = player.rect.x
+    old_y = player.rect.y
+
+    if keys[pygame.K_LEFT]:
+        player.rect.x -= player.speedx
+    if keys[pygame.K_RIGHT]:
+        player.rect.x += player.speedx
+    if keys[pygame.K_UP]:
+        player.rect.y -= player.speedy
+    if keys[pygame.K_DOWN]:
+        player.rect.y += player.speedy
+    if keys[pygame.K_SPACE] and not player.is_jumping:
+        pass
+
+    for entity in collidable_enties:
+        if entity is player:
+            continue
+        if player.rect.colliderect(entity.rect):
+            player.rect.x = old_x
+            player.rect.y = old_y
+            logger.warn(f"{player=} collided with {entity=}")
+            return
 
 
 pygame.init()
@@ -8,13 +39,31 @@ screen = pygame.display.set_mode((800, 600))
 # screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
 running = True
-one = Entity(speedx=1,speedy=1)
-static_entity = StaticEntity()
+player = MovableEntity(
+        Rect(150, 150, 30, 30),
+        speedx=1, speedy=1,
+        color=(255, 15, 192))
+
+ground_rect = Rect(0, 575, 800, 5)
+rect_1 = Rect(150, 450, 200, 25)
+rect_2 = Rect(350, 500, 200, 25)
+rect_3 = Rect(650, 375, 200, 25)
+
+BLUE = (0, 0, 255)
+
+ground = StaticEntity(ground_rect, color=BLUE)
+plataform_1 = StaticEntity(rect_1, color=BLUE)
+plataform_2 = StaticEntity(rect_2, color=BLUE)
+plataform_3 = StaticEntity(rect_3, color=BLUE)
+
 
 entities_list = [
-        one,
-        static_entity
-        ]
+    player,
+    ground,
+    plataform_1,
+    plataform_2,
+    plataform_3,
+]
 
 while running:
 
@@ -22,9 +71,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((255, 255, 255))
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    screen.fill((0, 0, 0))
 
+    keyboard_movement(player, collidable_enties=entities_list)
     for entity in entities_list:
         entity.draw(screen)
         entity.update()
