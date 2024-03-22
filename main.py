dcc4ffeea944a897077953dcd67d0c6a77870ff6
bhunao <bhunao@gmail.com>
2024-03-22 18:13:23 -0300

    tower defense?

--- main.py
+++ main.py
@@ -21,11 +21,39 @@ class C:
     BEGE = 15
 
 
+SPRITE = Tuple[int, int, int, int, int]
+
+
+def sprite(tile_x, tile_y, img=0, size=16, null_color=C.DARKBLUE) -> SPRITE:
+    return (img, tile_x * size, tile_y * size, size, size, null_color)
+
+
+class Sprite:
+    TREE = sprite(0, 1, img=2)
+    HOUSE = sprite(1, 2, img=2)
+    TOWER = sprite(2, 0, img=2)
+
+    # colunm 1
+    BLANK = sprite(0, 0)
+    FARM = sprite(0, 2)
+    ANVIL = sprite(0, 3)
+    # colunm 2
+    WALL = sprite(1, 0)
+
+    TEST = sprite(2, 1, img=2)
+
+
+HOUSES = [Sprite.HOUSE, Sprite.WALL, Sprite.FARM, Sprite.ANVIL, Sprite.TOWER]
+HOUSES = []
+for x in range(10):
+    for y in range(10):
+        HOUSES.append((2, x * 16, y * 16, 16, 16, C.DARKBLUE))
+
 ENTITY = Dict[str, Any]
 VEC2 = Tuple[int, int]
 
-WIDTH = 600
-HEIGHT = 400
+WIDTH = 320
+HEIGHT = 240
 
 N_BOX = 50
 
@@ -44,11 +72,9 @@ def move_rand(ent: ENTITY) -> None:
 def collision(entity1, entity2):
     # Get the center positions and half sizes of the entities
     entity1_pos = entity1["pos"]
-    entity1_half_w, entity1_half_h = entity1["size"][0] / \
-        2, entity1["size"][1] / 2
+    entity1_half_w, entity1_half_h = entity1["size"][0] / 2, entity1["size"][1] / 2
     entity2_pos = entity2["pos"]
-    entity2_half_w, entity2_half_h = entity2["size"][0] / \
-        2, entity2["size"][1] / 2
+    entity2_half_w, entity2_half_h = entity2["size"][0] / 2, entity2["size"][1] / 2
 
     return (
         abs(entity1_pos[0] - entity2_pos[0]) < entity1_half_w + entity2_half_w
@@ -58,50 +84,33 @@ def collision(entity1, entity2):
 
 class Screen:
     def __init__(self):
-        pyxel.init(WIDTH, HEIGHT, title="My Pyxel Game", fps=60)
+        pyxel.init(WIDTH, HEIGHT, title="My Pyxel Game", fps=60, display_scale=3)
         self.load_assets()  # Load assets before starting the game loop
         pyxel.mouse(True)
 
         self.entities: Dict[str, List[ENTITY]] = dict()
-        self.entities["player"] = [new(color=C.WHITE)]
+        self.map: Dict[Tuple[int, int], Any] = dict()
 
-        self.entities["enemy"] = []
-        # for _ in range(35):
-        #     x = pyxel.rndi(0, WIDTH)
-        #     y = pyxel.rndi(0, HEIGHT)
-        #     entity = new(pos=(x, y), color=C.PINK)
-        #     self.entities["enemy"].append(entity)
-
-        self.entities["pink"] = []
-        for _ in range(N_BOX):
-            x = pyxel.rndi(0, WIDTH)
-            y = pyxel.rndi(0, HEIGHT)
-            entity = new(pos=(x, y), color=C.PINK)
-            self.entities["pink"].append(entity)
-
-        self.entities["blue"] = []
-        for _ in range(N_BOX):
-            x = pyxel.rndi(0, WIDTH)
-            y = pyxel.rndi(0, HEIGHT)
-            entity = new(pos=(x, y), color=C.BLUE)
-            self.entities["blue"].append(entity)
-
-        self.entities["green"] = []
-        for _ in range(N_BOX):
-            x = pyxel.rndi(0, WIDTH)
-            y = pyxel.rndi(0, HEIGHT)
-            entity = new(pos=(x, y), color=C.GREEN)
-            self.entities["green"].append(entity)
+        self.house_n = 0
 
         pyxel.run(self.update, self.draw)  # Start the game loop
 
     # Load your image and sound assets (optional)
     def load_assets(self):
         # Use pyxel.img(x) and pyxel.sound(x) to load images and sounds
-        # For example:
-        pyxel.images[0].load(0, 0, "my_image.png")
+        pyxel.load("src.pyxres")
+        # pyxel.images[2].load(0, 0, "my_image.png")
+        pyxel.images[2].load(0, 0, "imgs/c1.png")
         # pyxel.sound(0).set("my_sound.wav", loop=True)
         pass
+
+    def mouse(self):
+        self.house_n += pyxel.mouse_wheel
+        if pyxel.btn(pyxel.MOUSE_BUTTON_LEFT):
+            x = pyxel.mouse_x // 16
+            y = pyxel.mouse_y // 16
+            i = self.house_n % len(HOUSES)
+            self.map[(x, y)] = HOUSES[i]
 
     def keyboard(self, player: ENTITY):
         if pyxel.btn(pyxel.KEY_Q):
@@ -122,48 +131,31 @@ class Screen:
         player["pos"] = x, y
 
     def update(self):
-        player = self.entities["player"][0]
-        self.keyboard(player)
-
-        for entity in self.entities["pink"]:
-            for entity2 in self.entities["green"]:
-                if collision(entity, entity2):
-                    entity2["color"] = C.PINK
-
-        for entity in self.entities["green"]:
-            for entity2 in self.entities["blue"]:
-                if collision(entity, entity2):
-                    entity2["color"] = C.GREEN
-
-        for entity in self.entities["blue"]:
-            for entity2 in self.entities["pink"]:
-                if collision(entity, entity2):
-                    entity2["color"] = C.BLUE
+        self.mouse()
 
     def draw(self):
-        pyxel.cls(C.DARKBLUE)
+        pyxel.cls(C.BLACK)
 
-        player = self.entities["player"][0]
-        x, y = player["pos"]
-        w, h = player["size"]
-        color = player["color"]
+        m_x = pyxel.mouse_x // 16
+        m_y = pyxel.mouse_y // 16
+        colors = [C.WHITE, C.GREEN, C.PURPLE, C.MEDIUMBLUE, C.ORANGE]
+        BOX_SIZE = 16
 
-        pyxel.rect(x, y, w, h, color)
+        for pos, sprite in self.map.items():
+            pyxel.blt(pos[0] * BOX_SIZE, pos[1] * BOX_SIZE, *sprite)
 
-        for name in ("pink", "green", "blue"):
-            for entity in self.entities[name]:
-                move_rand(entity)
-                x, y = entity["pos"]
-                w, h = entity["size"]
-                color = entity["color"]
-                pyxel.rect(x, y, w, h, color)
-                # if collision(entity, player):
-                #     entity["color"] = C.ORANGE
+        i = pyxel.frame_count % len(colors)
+        ii = self.house_n % len(HOUSES)
 
-        pyxel.bltm(0, 0, 0, 8, 8, 8, 8, 3)
-        pyxel.text(100, 20, str(len(self.entities["pink"])), C.PINK)
-        pyxel.text(100, 25, str(len(self.entities["green"])), C.GREEN)
-        pyxel.text(100, 30, str(len(self.entities["blue"])), C.BLUE)
+        pyxel.rectb(m_x * 16, m_y * 16, BOX_SIZE, BOX_SIZE, colors[i])
+        pyxel.blt(m_x * 16, m_y * 16, *HOUSES[ii])
+
+        mouse_tile = (
+            f"{pyxel.mouse_x // 16}\n{pyxel.mouse_y // 16}\n{pyxel.mouse_wheel}"
+        )
+        pyxel.text(0, 0, mouse_tile, C.PINK)
+        pyxel.blt(100, 100, *Sprite.TOWER)
+        pyxel.rectb(100, 100, BOX_SIZE, BOX_SIZE, colors[i])
 
 
 if __name__ == "__main__":
